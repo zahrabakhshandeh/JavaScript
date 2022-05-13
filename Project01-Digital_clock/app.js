c6194a01bef601574c04ee12173fcab30c9fef7d
zahrabakhshandeh <zahra.bakhshandeh1@gmail.com>
2022-05-13 12:58:57 +0430

    date

--- Project01-Digital_clock/app.js
+++ Project01-Digital_clock/app.js
@@ -22,4 +22,5 @@ setInterval(()=>{
   clockElement.textContent = clock;
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const curDay = days[now.getDay()];
 });
