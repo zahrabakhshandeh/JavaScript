3294786f827366d05c14e01987850cbd683320b7
zahrabakhshandeh <zahra.bakhshandeh1@gmail.com>
2022-05-13 10:58:28 +0430

    first commit

---
+++ Project01-Digital_clock/app.js
@@ -0,0 +1,25 @@
+setInterval(()=>{
+  let clockElement= document.querySelector(".clock");
+  const now = new Date();
+  let hour = now.getHours();
+  let minut = now.getMinutes();
+  let second = now.getSeconds();
+  let dayNight = "AM";
+  if(hour > 12){
+    dayNight = "PM";
+    hour = hour - 12;
+  }
+  if(hour < 10){
+    hour = "0" + hour;
+  }
+  if(minut < 10){
+    minut = "0" + minut;
+  }
+  if(second < 10){
+    second = "0" + second;
+  }
+  const clock = hour+": "+minut+": "+second+" "+dayNight;
+  clockElement.textContent = clock;
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+});
