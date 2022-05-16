860e5962e611567d7a4e6a2db038a132fd0ba9b4
zahrabakhshandeh <zahra.bakhshandeh1@gmail.com>
2022-05-17 02:52:31 +0430

    date added

--- Project01-Digital_clock/app.js
+++ Project01-Digital_clock/app.js
@@ -1,5 +1,6 @@
 setInterval(()=>{
   let clockElement= document.querySelector(".clock");
+  let dateElement= document.querySelector(".date");
   const now = new Date();
   let hour = now.getHours();
   let minut = now.getMinutes();
@@ -22,4 +23,10 @@ setInterval(()=>{
   clockElement.textContent = clock;
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const curWeekDay = days[now.getDay()];
+  const curDay = now.getDate();
+  const month = months[now.getMonth()];
+  const year = now.getFullYear();
+  const date = curWeekDay + ", " + curDay + " " + month + " " + year;
+  dateElement.textContent = date;
 });
